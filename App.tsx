ed32cbe144f0969446ce07617fae4a22bb313ac4
Gabriel <maykongfsilva@gmail.com>
2025-06-11 13:37:49 -0300

    subindo novas att

--- App.tsx
+++ App.tsx
@@ -1,16 +1,8 @@
-import 'react-native-gesture-handler';  // corrigido import para string sem ./ (mais padrão)
-import React, { JSX } from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
 import Routes from './src/routes/index.routes';
-
-export default function App(): JSX.Element {
-  return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({});
+export default () => (
+  <NavigationContainer>
+    <Routes />
+  </NavigationContainer>
+);
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,20 +1,27 @@
-import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { initializeApp, getApps } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence, getAuth } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9szN7cz3aaekk83JSMhg_SIMQ-jaYAUM",
-  authDomain: "listafacilapp-a0949.firebaseapp.com",
-  projectId: "listafacilapp-a0949",
-  storageBucket: "listafacilapp-a0949.appspot.com", // corrigido para o padrão
-  messagingSenderId: "884364349083",
-  appId: "1:884364349083:web:d0d7e8fe8152b17ce3003e"
+  apiKey: 'AIzaSyA9szN7cz3aaekk83JSMhg_SIMQ-jaYAUM',
+  authDomain: 'listafacilapp-a0949.firebaseapp.com',
+  projectId: 'listafacilapp-a0949',
+  storageBucket: 'listafacilapp-a0949.appspot.com',
+  messagingSenderId: '884364349083',
+  appId: '1:884364349083:web:d0d7e8fe8152b17ce3003e'
 };
 
-const app = initializeApp(firebaseConfig);
+let app;
+let auth;
 
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+} else {
+  app = getApps()[0];
+  auth = getAuth(app);
+}
 
-export { auth, app, firebaseConfig };
+export { app, auth };
